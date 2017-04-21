5b9900945fbc3fd6fb831965562d8b42496c211f
vittayakorn <witthayakon@gmail.com>
2017-04-21 14:27:47 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -13,6 +13,7 @@ if (!is_null($events['events'])) {
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
 			$text = $event['message']['text'];
+			$text = $text.' หรา';
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
