d6c011096d02797a899746c57890c9bd03cd9fe0
vittayakorn <witthayakon@gmail.com>
2017-04-21 14:17:03 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,6 +1,8 @@
 <?php
 $access_token = '2Jzs//UusGskjjlWxp0sbItd2EiaRejHjOkA4xabVoxVvlqDU7wPCNA2eUA0QIs2RITMSs1FIc4hPNtyt9DZ8XMesX0OsGm+9NhoZYp/hFf3lxXTWruLbJE5F1bpNerhxfwZMpB6v5u1cojixCw7XAdB04t89/1O/w1cDnyilFU=';
 
+
+
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
@@ -19,7 +21,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => $text || 'ว่ะ'
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
